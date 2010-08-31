508fc3cf1dc46eafd5db72341bd1848d558762fe
Simon Gregory <simon@helvector.org>
2010-09-01 00:06:56 +0100

    • Refactored tool_opts to reduce the Flog score.
    Apparently this is a good thing.

--- lib/hel/opts/tool_opts.rb
+++ lib/hel/opts/tool_opts.rb
@@ -8,18 +8,31 @@ require 'optparse'
 #
 class ToolOpts
 
+  #
+  # The name of the tool, as invoked on the command line.
+  #
   def self.name
     HelTool::NAME
   end
 
+  #
+  # A basic description of the tools use.
+  #
   def self.description
     'Helvector Tool'
   end
 
+  #
+  # A version string to describe the version of the tool these options are
+  # designed to invoke.
+  #
   def self.version
     HelTool::VERSION::STRING
   end
 
+  #
+  # Default configuration hash.
+  #
   def self.default_config
     {
       :src => ".",
@@ -30,45 +43,68 @@ class ToolOpts
   end
 
   #
-  # http://ruby-doc.org/stdlib/libdoc/optparse/rdoc/classes/OptionParser.html
+  # Create and return the options parser with the default header.
+  #
+  def self.create_parser
+    op = OptionParser.new
+    op.banner = "Usage: #{name} [options]"
+
+    op.separator ""
+    op.separator "Options:"
+    op
+  end
+
+  #
+  # Add all mandatory arguments to the options parser.
+  #
+  def self.add_mandatory(op,config)
+  end
+
+  #
+  # Add all optional arguments to the options parser.
+  #
+  def self.add_optional(op,config)
+    op.on("-s", "--source [PATH]", String, "Path to source folder, defaults to current directory.") do |v|
+      config[:src] = v
+    end
+
+    op.on("-o", "--output [FILE PATH]", String, "Path to output file, defaults to #{config[:output]}") do |v|
+      config[:output] = v
+    end
+
+    op.on("-v", "--verbose", "Run verbosely") do |v|
+      config[:verbose] = v
+    end
+
+    op.on("--silent", "Supress all output.") do |v|
+      config[:silent] = v
+    end
+  end
+
+  def self.add_tail(op,out)
+    op.on_tail("-h", "--help", "Show this help message") do
+      out.puts op
+      exit
+    end
+
+    op.on_tail("--version", "Show version") do
+      out.puts "#{description} version #{version}"
+      exit
+    end
+  end
+
+  #
+  # Parse the arugments and return a config hash.
   #
   def self.parse(args,out=STDOUT)
-    config = default_config
 
-    options = OptionParser.new do |opts|
-      opts.banner = "Usage: #{name} [options]"
+    config = default_config()
+    options = create_parser()
 
-      opts.separator ""
-      opts.separator "Options:"
+    add_mandatory(options,config)
+    add_optional(options,config)
 
-      opts.on("-s", "--source [PATH]", String,
-              "Path to source folder, defaults to current directory.") do |v|
-        config[:src] = v
-      end
-
-      opts.on("-o", "--output [PATH]", String,
-              "Path to output XML file, defaults to #{config[:output]}") do |v|
-        config[:output] = v
-      end
-
-      opts.on("-v", "--verbose", "Run verbosely") do |v|
-        config[:verbose] = v
-      end
-
-      opts.on("--silent", "Supress all output.") do |v|
-        config[:silent] = v
-      end
-
-      opts.on_tail("-h", "--help", "Show this message") do
-        out.puts opts
-        exit
-      end
-
-      opts.on_tail("--version", "Show version") do
-        out.puts "#{description} version #{version}"
-        exit
-      end
-    end
+    add_tail(options,out)
 
     options.parse!(args)
 
